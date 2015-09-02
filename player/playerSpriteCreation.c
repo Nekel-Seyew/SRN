d7fadee035de83a49916c14a1577b00860b6aec1
Kyle Sweeney <kylemdsweeney@gmail.com>
2015-09-01 20:56:32 -0700

    made Changes, to allow for better game mode switching

--- player/playerSpriteCreation.c
+++ player/playerSpriteCreation.c
@@ -11,18 +11,18 @@ int player_sprite_creation_draw(WINDOW* win){
 	box(win,0,0);
 	int i=0;
 	//it's row major
-	mvaddch(1,32,ACS_ULCORNER);
-	mvaddch(1,46,ACS_URCORNER);
+	mvwaddch(win,1,32,ACS_ULCORNER);
+	mvwaddch(win,1,46,ACS_URCORNER);
 	for(i=0; i<13;i++){
-		mvaddch(1,33+i,ACS_HLINE);
-		mvaddch(9,33+i,ACS_HLINE);
+		mvwaddch(win,1,33+i,ACS_HLINE);
+		mvwaddch(win,9,33+i,ACS_HLINE);
 	}
 	for(i=0;i<7;i++){
-		mvaddch(2+i,32,ACS_VLINE);
-		mvaddch(2+i,46,ACS_VLINE);
+		mvwaddch(win,2+i,32,ACS_VLINE);
+		mvwaddch(win,2+i,46,ACS_VLINE);
 	}
-	mvaddch(9,32,ACS_LLCORNER);
-	mvaddch(9,46,ACS_LRCORNER);
+	mvwaddch(win,9,32,ACS_LLCORNER);
+	mvwaddch(win,9,46,ACS_LRCORNER);
 	return 0;
 }
 int player_sprite_creation_update(int charinput){
