5a413f3fcdf46069625173647262d2a92cf4e1d0
Kyle Sweeney <ksweene3@nd.edu>
2017-01-01 03:59:21 -0500

    fixed test.c seg-fault

--- graphics/test.c
+++ graphics/test.c
@@ -6,12 +6,16 @@
 
 int main(){
 	struct sprite* sprite;
+	sprite = malloc(sizeof(struct sprite));
 	sprite->height = 7;
 	sprite->width = 13;
 	sprite->sprite="...........................................................................................";
 	int a[91] = {4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4};
 	sprite->color =a ;
+	printf("Printing JSON version of sprite\n");
+	printf("sprite: %s\n",sprite->sprite);
 	char* json = sprite_to_JSON(sprite);
 	printf("%s\n",json);
+	free(sprite);
 	return 0;
 }
